db803928a88720abd259ca321f2ffb234f1fce00
Zeeta2 <felijanezeewin@gmail.com>
2026-06-14 23:34:11 +0530

    Add files via upload

---
+++ Cognizant_DeepSkilling_23/PL SQL Programming/Exercise 1 Control Structures/Scenario2.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Customers(
+  Customer_id NUMBER PRIMARY KEY,
+  name VARCHAR2(50),
+  balance NUMBER,
+  IsVIP VARCHAR2(5)
+);
+INSERT INTO Customers VALUES(1,'John',8000,'FALSE');
+INSERT INTO Customers VALUES(2,'Mary',12000,'FALSE');
+INSERT INTO Customers VALUES(3,'David',15000,'FALSE');
+
+--BEFORE
+SELECT * FROM Customers;
+BEGIN
+  FOR c IN(
+    SELECT Customer_id,balance
+    FROM Customers
+  )
+  LOOP
+    IF c.balance>10000 THEN
+      UPDATE Customers
+      SET IsVIP = 'TRUE'
+      WHERE Customer_id=c.Customer_id;
+    END IF;
+  END LOOP;
+  COMMIT;
+END;
+/
+SELECT * FROM Customers;
