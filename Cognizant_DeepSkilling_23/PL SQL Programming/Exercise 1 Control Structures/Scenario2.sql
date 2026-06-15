f844eeb8b4f9c2e8898334d7962e3033392bc4a2
Zeeta2 <felijanezeewin@gmail.com>
2026-06-15 23:45:52 +0530

    Update Scenario2.sql

--- Cognizant_DeepSkilling_23/PL SQL Programming/Exercise 1 Control Structures/Scenario2.sql
+++ Cognizant_DeepSkilling_23/PL SQL Programming/Exercise 1 Control Structures/Scenario2.sql
@@ -2,31 +2,42 @@
 -- o	Question: Write a PL/SQL block that iterates through all customers and sets
 --a flag IsVIP to TRUE for those with a balance over $10,000.
 
-CREATE TABLE Customers(
-  Customer_id NUMBER PRIMARY KEY,
-  name VARCHAR2(50),
-  balance NUMBER,
-  IsVIP VARCHAR2(5)
+
+CREATE TABLE Customers (
+    CustomerID NUMBER PRIMARY KEY,
+    Name VARCHAR2(100),
+    DOB DATE,
+    Balance NUMBER,
+    LastModified DATE,
+    IsVIP VARCHAR2(5)
 );
-INSERT INTO Customers VALUES(1,'John',8000,'FALSE');
-INSERT INTO Customers VALUES(2,'Mary',12000,'FALSE');
-INSERT INTO Customers VALUES(3,'David',15000,'FALSE');
+
+INSERT INTO Customers
+VALUES (1, 'John Doe', TO_DATE('1985-05-15','YYYY-MM-DD'), 8000, SYSDATE, 'FALSE');
+
+INSERT INTO Customers
+VALUES (2, 'Jane Smith', TO_DATE('1990-07-20','YYYY-MM-DD'), 12000, SYSDATE, 'FALSE');
+
+INSERT INTO Customers
+VALUES (3, 'Mary Senior', TO_DATE('1950-01-01','YYYY-MM-DD'), 15000, SYSDATE, 'FALSE');
+
+COMMIT;
 
 --BEFORE
-SELECT * FROM Customers;
+SELECT CustomerID ,Name,Balance,IsVIP FROM Customers;
 BEGIN
   FOR c IN(
-    SELECT Customer_id,balance
+    SELECT CustomerID,Balance
     FROM Customers
   )
   LOOP
     IF c.balance>10000 THEN
       UPDATE Customers
       SET IsVIP = 'TRUE'
-      WHERE Customer_id=c.Customer_id;
+      WHERE CustomerID=c.CustomerID;
     END IF;
   END LOOP;
   COMMIT;
 END;
 /
-SELECT * FROM Customers;
+SELECT CustomerID ,Name,Balance,IsVIP FROM Customers;
