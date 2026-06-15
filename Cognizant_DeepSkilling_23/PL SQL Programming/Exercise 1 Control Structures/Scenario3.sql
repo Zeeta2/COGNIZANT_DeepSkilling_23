d3fc39e30f87fa4696a94bb71cf288589f914ea7
Zeeta2 <felijanezeewin@gmail.com>
2026-06-15 23:54:07 +0530

    Update Scenario3.sql

--- Cognizant_DeepSkilling_23/PL SQL Programming/Exercise 1 Control Structures/Scenario3.sql
+++ Cognizant_DeepSkilling_23/PL SQL Programming/Exercise 1 Control Structures/Scenario3.sql
@@ -4,42 +4,59 @@
 --and prints a reminder message for each customer.
 
 CREATE TABLE Customers (
-    customer_id NUMBER PRIMARY KEY,
-    name VARCHAR2(50)
+    CustomerID NUMBER PRIMARY KEY,
+    Name VARCHAR2(100),
+    DOB DATE,
+    Balance NUMBER,
+    LastModified DATE
 );
 
 CREATE TABLE Loans (
-    loan_id NUMBER PRIMARY KEY,
-    customer_id NUMBER,
-    due_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    LoanID NUMBER PRIMARY KEY,
+    CustomerID NUMBER,
+    LoanAmount NUMBER,
+    InterestRate NUMBER,
+    StartDate DATE,
+    EndDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
 
-INSERT INTO Customers VALUES (1,'John');
-INSERT INTO Customers VALUES (2,'David');
-INSERT INTO Customers VALUES (3,'Mary');
+INSERT INTO Customers
+VALUES (1, 'John Doe', TO_DATE('1985-05-15', 'YYYY-MM-DD'), 1000, SYSDATE);
 
-INSERT INTO Loans VALUES (101,1,SYSDATE+40);
-INSERT INTO Loans VALUES (102,2,SYSDATE+20);
-INSERT INTO Loans VALUES (103,3,SYSDATE+10);
+INSERT INTO Customers
+VALUES (2, 'Jane Smith', TO_DATE('1990-07-20', 'YYYY-MM-DD'), 1500, SYSDATE);
+
+INSERT INTO Customers
+VALUES (3, 'Mary Senior', TO_DATE('1950-01-01', 'YYYY-MM-DD'), 2000, SYSDATE);
+
+INSERT INTO Loans
+VALUES (1, 1, 5000, 5, SYSDATE, SYSDATE + 40);
+
+INSERT INTO Loans
+VALUES (2, 2, 7000, 6, SYSDATE, SYSDATE + 20);
+
+INSERT INTO Loans
+VALUES (3, 3, 10000, 7, SYSDATE, SYSDATE + 10);
+
+COMMIT;
 SELECT * FROM Customers;
 SELECT * FROM Loans;
-COMMIT;
 
 SET SERVEROUTPUT ON;
 BEGIN 
   FOR l IN(
-    SELECT c.name,l.loan_id,l.due_date
+    SELECT c.name,l.loanID,l.EndDate
     FROM Customers c
     JOIN Loans l
-    ON c.customer_id=l.customer_id
-    WHERE l.due_date BETWEEN SYSDATE AND SYSDATE+30
+    ON c.customerID=l.customerID
+    WHERE l.EndDate BETWEEN SYSDATE AND SYSDATE+30
   )
   LOOP
     DBMS_OUTPUT.PUT_LINE(
-      'Remainder: Dear '||l.name||
-      ', your loan ID '||l.loan_id||
-      ' is due on '||l.due_date
+      'Remainder: Dear '||l.Name||
+      ', your loan ID '||l.loanID||
+      ' is due on '||l.EndDate
     );
   END LOOP;
 END;
