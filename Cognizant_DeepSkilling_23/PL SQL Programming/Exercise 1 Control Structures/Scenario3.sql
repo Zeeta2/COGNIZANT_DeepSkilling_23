2f97bd1603500fb331668171737b967817abfd3a
Zeeta2 <felijanezeewin@gmail.com>
2026-06-15 20:47:20 +0530

    Update Scenario3.sql

--- Cognizant_DeepSkilling_23/PL SQL Programming/Exercise 1 Control Structures/Scenario3.sql
+++ Cognizant_DeepSkilling_23/PL SQL Programming/Exercise 1 Control Structures/Scenario3.sql
@@ -1,3 +1,8 @@
+--Scenario 3: The bank wants to send reminders to customers whose loans are due within the 
+--next 30 days.
+-- o	Question: Write a PL/SQL block that fetches all loans due in the next 30 days 
+--and prints a reminder message for each customer.
+
 CREATE TABLE Customers (
     customer_id NUMBER PRIMARY KEY,
     name VARCHAR2(50)
