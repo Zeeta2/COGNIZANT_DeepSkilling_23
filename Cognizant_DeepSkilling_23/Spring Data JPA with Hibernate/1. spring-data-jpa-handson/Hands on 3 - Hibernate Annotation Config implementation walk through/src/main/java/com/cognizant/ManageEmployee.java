84dd308b4dbab5958ab2e615adee16807af24501
Zeeta2 <felijanezeewin@gmail.com>
2026-06-25 22:47:32 +0530

    Add files via upload

---
+++ Cognizant_DeepSkilling_23/Spring Data JPA with Hibernate/1. spring-data-jpa-handson/Hands on 3 - Hibernate Annotation Config implementation walk through/src/main/java/com/cognizant/ManageEmployee.java
@@ -0,0 +1,69 @@
+package com.cognizant;
+
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class ManageEmployee {
+    private static SessionFactory factory;
+
+    public static void main(String[] args) {
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        ManageEmployee me = new ManageEmployee();
+
+        Integer emp1 = me.addEmployee("Zara", "Ali", 1000);
+        Integer emp2 = me.addEmployee("Daisy", "Das", 5000);
+        Integer emp3 = me.addEmployee("John", "Paul", 10000);
+
+        me.listEmployees();
+    }
+    public Integer addEmployee(String fname, String lname, int salary) {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Integer employeeID = null;
+
+        try {
+            tx = session.beginTransaction();
+            Employee employee = new Employee(fname, lname, salary);
+            employeeID = (Integer) session.save(employee);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return employeeID;
+    }
+
+    public void listEmployees() {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            List employees = session.createQuery("FROM Employee").list();
+
+            for (Iterator iterator = employees.iterator(); iterator.hasNext();) {
+                Employee employee = (Employee) iterator.next();
+                System.out.print("First Name: " + employee.getFirstName());
+                System.out.print("  Last Name: " + employee.getLastName());
+                System.out.println("  Salary: " + employee.getSalary());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+}
