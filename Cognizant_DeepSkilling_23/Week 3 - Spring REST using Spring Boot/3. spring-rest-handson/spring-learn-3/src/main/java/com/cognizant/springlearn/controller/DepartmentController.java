b50f7889de54e79f6454b46e94736f4b765e9811
Zeeta2 <felijanezeewin@gmail.com>
2026-07-09 15:45:08 +0530

    3. Spring rest handson

---
+++ Cognizant_DeepSkilling_23/Week 3 - Spring REST using Spring Boot/3. spring-rest-handson/spring-learn-3/src/main/java/com/cognizant/springlearn/controller/DepartmentController.java
@@ -0,0 +1,25 @@
+package com.cognizant.springlearn.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.model.Department;
+import com.cognizant.springlearn.service.DepartmentService;
+
+@RestController
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @GetMapping("/departments")
+    public ArrayList<Department> getAllDepartments() {
+
+        System.out.println("Department REST Service Invoked");
+
+        return departmentService.getAllDepartments();
+    }
+}
