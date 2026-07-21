89105aa11b1fb88db0ba63ec40aa1615ca607248
Zeeta2 <felijanezeewin@gmail.com>
2026-07-22 00:52:53 +0530

    Add files via upload

---
+++ Cognizant_DeepSkilling_23/Spring data JPA handson/H6/OrmLearnApplication.java
@@ -0,0 +1,49 @@
+package com.cognizant.ormlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.ormlearn.model.Employee;
+import com.cognizant.ormlearn.service.DepartmentService;
+import com.cognizant.ormlearn.service.EmployeeService;
+import com.cognizant.ormlearn.service.SkillService;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static EmployeeService employeeService;
+    private static DepartmentService departmentService;
+    private static SkillService skillService;
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                SpringApplication.run(
+                        OrmLearnApplication.class,
+                        args);
+
+        employeeService =
+                context.getBean(EmployeeService.class);
+
+        departmentService =
+                context.getBean(DepartmentService.class);
+
+        skillService =
+                context.getBean(SkillService.class);
+
+        testGetEmployee();
+    }
+
+    private static void testGetEmployee() {
+
+        Employee employee =
+                employeeService.get(1);
+
+        System.out.println(employee);
+
+        System.out.println(employee.getDepartment());
+
+        System.out.println(employee.getSkillList());
+    }
+}
