8a2624b47047da6fd7bc09f335b2b51b54dee24e
Zeeta2 <felijanezeewin@gmail.com>
2026-07-22 11:43:06 +0530

    Add files via upload

---
+++ Cognizant_DeepSkilling_23/Microservices/3. Microservices composite handson/api-gateway1/ApiGateway1Application.java
@@ -0,0 +1,12 @@
+package com.cognizant.apigateway1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateway1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateway1Application.class, args);
+    }
+}
